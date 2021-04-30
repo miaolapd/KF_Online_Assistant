07e7d4b9370e5372dbfedc6477feff99982e136e
Hazuki Kaguya <35645329+HazukiKaguya@users.noreply.github.com>
2021-04-30 14:07:51 +0900

    Update KfEmotion.user.js
    增加导出自定义贴纸功能

--- scripts/es5/KfEmotion.user.js
+++ scripts/es5/KfEmotion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        绯月表情增强插件
 // @namespace   https://greasyfork.org/users/5415
-// @version     6.0.0
+// @version     6.0.1
 // @author      eddie32
 // @description KF论坛专用的回复表情，插图扩展插件，在发帖时快速输入自定义表情和论坛BBCODE
 // @icon        https://sticker.inari.site/favicon.ico
@@ -24,7 +24,7 @@
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-var version = '6.0.0';
+var version = '6.0.1';
 // 网站是否为KfMobile
 var isKfMobile = typeof Info !== 'undefined' && typeof Info.imgPath !== 'undefined';
 
@@ -206,7 +206,7 @@ var getSubMenuHtml = function getSubMenuHtml() {
  * @param textArea 文本框
  */
 var createContainer = function createContainer(textArea) {
-    var $container = $('\n<div class="kfe-container">\n  <div class="kfe-menu">\n    <span title="made by eddie32 version ' + version + '; modified by \u55B5\u62C9\u5E03\u4E01\u3001mistakey" style="cursor: pointer;"><b>\u56E7\u2468</b></span>\n    ' + getSubMenuHtml() + '\n    <input type="button" class="kfe-user-add" value="\u6DFB\u52A0">\n    <input type="button" class="kfe-user-clr" value="\u6E05\u7A7A">\n    <span class="kfe-close-panel">[-]</span>\n  </div>\n</div>\n').insertBefore($(textArea));
+    var $container = $('\n<div class="kfe-container">\n  <div class="kfe-menu">\n    <span title="made by eddie32 version ' + version + '; modified by \u55B5\u62C9\u5E03\u4E01\u3001mistakey" style="cursor: pointer;"><b>\u56E7\u2468</b></span>\n    ' + getSubMenuHtml() + '\n    <span class="kfe-close-panel">[-]</span>\n   <input type="button" class="kfe-user-add" value="\u6DFB\u52A0">\n    <input type="button" class="kfe-user-out" value="\u5BFC\u51FA">\n    <input type="button" class="kfe-user-clr" value="\u6E05\u7A7A">\n     </div>\n</div>\n').insertBefore($(textArea));
     $container.on('click', '.kfe-sub-menu', function (e) {
         e.preventDefault();
         var $this = $(this);
@@ -231,7 +231,7 @@ var createContainer = function createContainer(textArea) {
         $('.kfe-zoom-in').remove();
     }).on('click', '.kfe-user-add', function (e) {
         e.preventDefault();
-        var userimgaddr = prompt("请输入要添加的贴纸的URL（添加后需刷新页面才能生效）:", "https://sticker.inari.site/inari.png");
+        var userimgaddr = prompt("请输入要添加的贴纸的URL，添加多个请用半角,隔开贴纸URL（添加后刷新页面生效）", "https://sticker.inari.site/inari.png");
         if (!userimgaddr) return;
 
         var userimgaddrmt = userimgaddr.split(',');
@@ -255,6 +255,14 @@ var createContainer = function createContainer(textArea) {
 
             userSmileList = [].concat(_toConsumableArray(userSmileList), addList);
             localStorage.setItem('userimgst', JSON.stringify(userSmileList));
+        }
+     }).on('click', '.kfe-user-out', function (e) {
+        e.preventDefault();
+        if (UserSmileList !="https://sticker.inari.site/null.jpg"){
+           prompt("自定义表情贴纸已导出，请复制",UserSmileList);
+        }
+        else {
+           alert("自定义表情贴纸为空！");
         }
     }).on('click', '.kfe-user-clr', function (e) {
         e.preventDefault();
